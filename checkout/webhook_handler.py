e46d3dd0db4c29a52ebe9112a49394bde2e0a5ce
Emilia Pearse <mili.pearse@gmail.com>
2024-07-08 16:28:20 +0000

    Deleted size in webhooks handler and fixed indentation

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -53,7 +53,7 @@ class StripeWH_Handler:
 
         # Get the Charge object
         stripe_charge = stripe.Charge.retrieve(
-        intent.latest_charge
+            intent.latest_charge
         )   
 
         billing_details = stripe_charge.billing_details 
@@ -86,7 +86,6 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
-                    user_profile=profile,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
                     country__iexact=shipping_details.address.country,
@@ -114,6 +113,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
@@ -134,14 +134,6 @@ class StripeWH_Handler:
                             quantity=item_data,
                         )
                         order_line_item.save()
-                    else:
-                        for quantity in item_data.items():
-                            order_line_item = OrderLineItem(
-                                order=order,
-                                product=product,
-                                quantity=quantity,
-                            )
-                            order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()
